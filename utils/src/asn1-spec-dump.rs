40c60a04dffe39c8cc6738c053f7c3e149d64bea
Josh Driver <keeperofdakeys@gmail.com>
2016-10-04 22:52:06 +1030

    Create a utility to dump an asn1 schema

---
+++ utils/src/asn1-spec-dump.rs
@@ -0,0 +1,43 @@
+extern crate asn1_parse;
+#[macro_use]
+extern crate nom;
+extern crate argparse;
+
+use std::io;
+use std::io::Read;
+use std::fs;
+use std::path::Path;
+use asn1_parse::parse::asn1_type_def;
+use argparse::{ArgumentParser, StoreOption};
+
+fn main() {
+  let opts = parse_args();
+
+  let bytes: Result<Vec<_>, _> = match opts.file {
+    Some(ref p) => io::BufReader::new(
+      fs::File::open(Path::new(p)).unwrap()
+    ).bytes().collect(),
+    None => io::stdin().bytes().collect(),
+  };
+  let buffer = bytes.unwrap();
+  println!("{:?}", many0!(buffer.as_slice(), asn1_type_def));
+}
+
+struct ProgOpts {
+  file: Option<String>,
+}
+
+fn parse_args() -> ProgOpts {
+  let mut opts = ProgOpts {
+    file: None,
+  };
+
+  {
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Dump ASN.1 specification files");
+    ap.refer(&mut opts.file)
+      .add_argument("file", StoreOption, "ASN.1 spec file to dump");
+    ap.parse_args_or_exit();
+  }
+  opts
+}
